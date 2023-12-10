cffba1ad85491ea23de312a2216620e32514d473
GhitescuCaterina <100339932+GhitescuCaterina@users.noreply.github.com>
2023-12-11 00:59:02 +0200

    am facut sarpele

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-# import turtle
 import random
-
 import pygame
 import sys
 from pygame.math import Vector2
+
+
+class SARPE:
+    def __init__(self):
+        self.corp = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+        self.directie = Vector2(1, 0)
+
+    def draw_snake(self):
+        for block in self.corp:
+            x_pozitie = int(block.x * cell_size)
+            y_pozitie = int(block.y * cell_size)
+            corp_rect = pygame.Rect(x_pozitie, y_pozitie, cell_size, cell_size)
+            pygame.draw.rect(screen, (58, 192, 100), corp_rect)
+
+    def move_snake(self):
+        copie_corp = self.corp[:-1]
+        copie_corp.insert(0, copie_corp[0] + self.directie)
+        self.corp = copie_corp[:]
 
 
 class MANCARE:
@@ -24,6 +40,10 @@ screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_si
 clock = pygame.time.Clock()
 
 fruit = MANCARE()
+sarpe = SARPE()
+
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE,150)
 
 # ecranul de joc
 while True:
@@ -31,7 +51,20 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == SCREEN_UPDATE:
+            sarpe.move_snake()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                sarpe.direction = Vector2(0, -1)
+            if event.key == pygame.K_d:
+                sarpe.direction = Vector2(1, 0)
+            if event.key == pygame.K_s:
+                sarpe.direction = Vector2(0, 1)
+            if event.key == pygame.K_a:
+                sarpe.direction = Vector2(-1, 0)
+
     screen.fill((175, 30, 70))
     fruit.draw_fruit()
+    sarpe.draw_snake()
     pygame.display.update()
     clock.tick(60)
