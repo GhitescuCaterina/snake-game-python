5d79bd81de402480ce31c070294b738b2daa1c5b
GhitescuCaterina <100339932+GhitescuCaterina@users.noreply.github.com>
2023-12-13 01:17:31 +0200

    logica jocului

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class SARPE:
     def __init__(self):
         self.corp = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
         self.directie = Vector2(1, 0)
+        self.bucata_noua = False
 
     def draw_snake(self):
         for block in self.corp:
@@ -17,20 +18,65 @@ class SARPE:
             pygame.draw.rect(screen, (58, 192, 100), corp_rect)
 
     def move_snake(self):
-        copie_corp = self.corp[:-1]
-        copie_corp.insert(0, copie_corp[0] + self.directie)
-        self.corp = copie_corp[:]
+        if self.bucata_noua:
+            copie_corp = self.corp[:]
+            copie_corp.insert(0, copie_corp[0] + self.directie)
+            self.corp = copie_corp[:]
+            self.bucata_noua = False
+        else:
+            copie_corp = self.corp[:-1]
+            copie_corp.insert(0, copie_corp[0] + self.directie)
+            self.corp = copie_corp[:]
+
+    def bucata_corp(self):
+        self.bucata_noua = True
 
 
 class MANCARE:
     def __init__(self):
-        self.x = random.randint(0, cell_number-1)
-        self.y = random.randint(0, cell_number-1)
-        self.pozitie = Vector2(self.x, self.y)
+        self.randomize()
 
     def draw_fruit(self):
-        fruit_rect = pygame.Rect(int(self.pozitie.x*cell_size), int(self.pozitie.y*cell_size), cell_size, cell_size)
+        fruit_rect = pygame.Rect(int(self.pozitie.x * cell_size), int(self.pozitie.y * cell_size), cell_size, cell_size)
         pygame.draw.rect(screen, (255, 30, 30), fruit_rect)
+
+    def randomize(self):
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pozitie = Vector2(self.x, self.y)
+
+
+def game_over():
+    pygame.quit()
+    sys.exit()
+
+
+class MAIN:
+    def __init__(self):
+        self.sarpe = SARPE()
+        self.mancare = MANCARE()
+
+    def update(self):
+        self.sarpe.move_snake()
+        self.coliziune()
+        self.pierdut()
+
+    def draw_everything(self):
+        self.mancare.draw_fruit()
+        self.sarpe.draw_snake()
+
+    def coliziune(self):
+        if self.mancare.pozitie == self.sarpe.corp[0]:
+            self.mancare.randomize()
+            self.sarpe.bucata_corp()
+
+    def pierdut(self):
+        if not 0 <= self.sarpe.corp[0].x < cell_number or not 0 <= self.sarpe.corp[0].y < cell_number:
+            game_over()
+
+        for block in self.sarpe.corp[1:]:
+            if block == self.sarpe.corp[0]:
+                game_over()
 
 
 pygame.init()
@@ -39,11 +85,10 @@ cell_number = 20
 screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 clock = pygame.time.Clock()
 
-fruit = MANCARE()
-sarpe = SARPE()
-
 SCREEN_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(SCREEN_UPDATE,150)
+pygame.time.set_timer(SCREEN_UPDATE, 150)
+
+main_game = MAIN()
 
 # ecranul de joc
 while True:
@@ -52,19 +97,18 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == SCREEN_UPDATE:
-            sarpe.move_snake()
+            main_game.update()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
-                sarpe.direction = Vector2(0, -1)
+                main_game.sarpe.directie = Vector2(0, -1)
             if event.key == pygame.K_d:
-                sarpe.direction = Vector2(1, 0)
+                main_game.sarpe.directie = Vector2(1, 0)
             if event.key == pygame.K_s:
-                sarpe.direction = Vector2(0, 1)
+                main_game.sarpe.directie = Vector2(0, 1)
             if event.key == pygame.K_a:
-                sarpe.direction = Vector2(-1, 0)
+                main_game.sarpe.directie = Vector2(-1, 0)
 
     screen.fill((175, 30, 70))
-    fruit.draw_fruit()
-    sarpe.draw_snake()
+    main_game.draw_everything()
     pygame.display.update()
     clock.tick(60)
